25afaff4bcde2db07ee64c3fee0095c8777e763a
Ruslan Naeltok <ruslannaeltok@gmail.com>
2019-10-02 01:22:07 +0300

    add build script

---
+++ build.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+start=$(date +%s.%N)
+
+describeComponent() { 
+	cd ..
+	cd $1
+	echo $1
+	git branch
+}
+
+case $1 in
+list-branches)
+	describeComponent "gps-users"
+	describeComponent "gps-tracker"
+	describeComponent "gps-gateway"
+	;;	
+all)
+	# Update deployment scenarios
+	git pull
+	# Build the users service
+	cd ../gps-users
+	git pull
+	./build.sh
+	# Build the tracker service
+	cd ../gps-tracker
+	git pull
+	./build.sh
+	# Return back to the current folder
+	cd ../gps-deploy
+	;;
+*)
+	echo "Usage:"
+	echo ""
+	echo "# To list all branches for local repositories before the build:"
+	echo "./build.sh list-branches"
+	echo ""
+	echo "# To build all components:"
+	echo "./build.sh all"
+	echo ""
+	;;
+esac
+
+dur=$(echo "$(date +%s.%N) - $start" | bc)
+
+LC_NUMERIC="en_US.UTF-8" printf "\n-----Execution time: %.6f seconds-----\n\n" $dur
